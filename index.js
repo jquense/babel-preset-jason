21aae1e070d65a7be930e3e0eccdc19800dd9125
Jason Quense <monastic.panic@gmail.com>
2016-10-14 10:26:53 -0400

    options

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 module.exports = {
   presets: [
-    require("babel-preset-es2015-loose"),
+    [require("babel-preset-es2015"), { loose: true }],
     require("babel-preset-react"),
-    require("babel-preset-stage-0"),
+    require("babel-preset-stage-1"),
   ],
   plugins: [
     require("babel-plugin-add-module-exports"),
